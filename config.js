e79fa79f5747e52cea7e750b2b6f64b5827c4c2f
Yemen <166258777+Sn4a5s6h@users.noreply.github.com>
2025-08-26 22:53:08 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
-{
- export const "botToken": "6767498865:AAFibdms3ba_9bH8vqD6X92DrkiRHkPEn2w",
-export const "chatId": "-1002819450502"
-}
+export const botToken = "6767498865:AAFibdms3ba_9bH8vqD6X92DrkiRHkPEn2w";
+export const chatId = "-1002819450502";
